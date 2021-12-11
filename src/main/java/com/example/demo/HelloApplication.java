e9af9a22c318eef9733e10250313fca5f75faf7e
DavidGabelgaard <davi493c@edu.easj.dk>
2021-12-11 21:10:13 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400 , 600);
+        stage.setTitle("MobilePay Start Page");
+        stage.setScene(scene);
+        stage.setResizable(false);
+
+
+
+
+        stage.show();
+        scene.lookup("HBox").requestFocus();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+
+
+
+
+}
+
+
